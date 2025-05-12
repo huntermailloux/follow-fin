cbcbfd03896177248c53457dad75c838d5f0105f
Hunter Mailloux <huntermmailloux@gmail.com>
2025-05-12 18:16:10 -0400

    Adding home and admin components + authentication with Auth0

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideAuth0({
+      domain: 'followfin.ca.auth0.com',
+      clientId: '1ooECMr4GdCYMJFJpjiWxDkOIdT23OMq',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { AuthGuard } from '@auth0/auth0-angular';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'admin',
+        component: AdminComponent,
+        canActivate: [AuthGuard]
+    },
+    { 
+        path: '**',
+        redirectTo: ''
+    }
+];
